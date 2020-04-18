21525c89348587620aa440b25c163dc96a20e40b
Sandra Jasionowska <s>
2020-04-18 14:54:41 +0200

    Added basic movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private float walkingSpeed = 2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	private float runningSpeed = 3f;
+
+	private Rigidbody2D rigidbody;
+
+	private void Start()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		Move();
+	}
+	
+	
+	
+	private void Move()
+	{
+		var walkingDirection = Vector3.zero;
+
+		walkingDirection += Vector3.up * Input.GetAxis("Vertical");
+		walkingDirection += Vector3.right * Input.GetAxis("Horizontal");
+
+		walkingDirection = walkingDirection.normalized;
+		walkingDirection *= Input.GetKey(KeyCode.LeftShift) ? runningSpeed : walkingSpeed;
+
+		rigidbody.MovePosition(transform.position + walkingDirection * Time.fixedDeltaTime);
+	}
 }
