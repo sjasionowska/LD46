1d0920f61f2698eb116a3042d5ea7a930a1e306a
Sandra Jasionowska <s>
2020-04-18 17:31:17 +0200

    Basic AI for the Enemy added. Basic animations for Player added.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+
+    [SerializeField]
+    private float attackDistance = 2f;
+
+    private float attackDamage = 1f;
+
+    private Vector2 targetPosition;
+
+    private Vector2 direction;
+    
+#pragma warning disable 108,114
+    // ReSharper disable once IdentifierTypo
+    private Rigidbody2D rigidbody;
+#pragma warning restore 108,114
+
+    private Player targetPlayer;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+    
+    private IEnumerator ChangeTargetPositionCoroutine()
+    {
+        while(true)
+        {
+            targetPosition = (Vector2)transform.position + Random.insideUnitCircle * 10f;
+            yield return new WaitForSeconds(Random.Range(5, 10));
+        }
+    }
+    
+    
+    private void Update ()
+    {
+       
+        Attack();
+    }
+    
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (targetPlayer != null) targetPosition = targetPlayer.transform.position;
+
+        else return;
+
+        direction = targetPosition - rigidbody.position;
+        var targetVelocity = direction.normalized;
+        
+        rigidbody.MovePosition(targetPosition * (speed * Time.fixedDeltaTime));
+
+        
+        rigidbody.MovePosition(rigidbody.position + direction * (speed * Time.fixedDeltaTime));
+       
+        // rigidbody.velocity = Vector3.Lerp(
+        //     rigidbody.velocity,
+        //     targetVelocity,
+        //     Time.deltaTime / 2f);
+
+        // transform.right = (Vector2)direction;
+    }
+
+    
+
+    private void Attack()
+    {
+        if (targetPlayer == null)
+            return;
+
+        var distance = (targetPlayer.transform.position - transform.position).magnitude;
+
+        if (distance > attackDistance)
+            return;
+
+        Debug.Log("attack " + Time.deltaTime);
+        
+        
+        // targetPlayer.GetComponent<Entity>().Health -= AttackDamage * Time.deltaTime;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.gameObject.GetComponent<Player>();
+
+        if (player != null)
+            targetPlayer = player;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var player = collision.gameObject.GetComponent<Player>();
+
+        if (player != null)
+            targetPlayer = null;
+    }
+    
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,30 +13,55 @@ public class Player : MonoBehaviour
 	[SerializeField]
 	private float runningSpeed = 3f;
 
+	private Animator animator;
+
+#pragma warning disable 108,114
+	// ReSharper disable once IdentifierTypo
 	private Rigidbody2D rigidbody;
+#pragma warning restore 108,114
+
+	private Vector2 movement;
+
+	private float currentSpeed;
+
+	private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+
+	private static readonly int Vertical = Animator.StringToHash("Vertical");
+
+	private static readonly int Speed = Animator.StringToHash("Speed");
 
 	private void Start()
 	{
 		rigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+		currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runningSpeed : walkingSpeed;
+		
+		animator.SetFloat(Horizontal, movement.x);
+		animator.SetFloat(Vertical, movement.y);
+		animator.SetFloat(Speed, movement.sqrMagnitude);
 	}
 
 	private void FixedUpdate()
 	{
-		Move();
+		rigidbody.MovePosition(rigidbody.position + movement * (currentSpeed * Time.fixedDeltaTime));
 	}
-	
-	
-	
-	private void Move()
-	{
-		var walkingDirection = Vector3.zero;
 
-		walkingDirection += Vector3.up * Input.GetAxis("Vertical");
-		walkingDirection += Vector3.right * Input.GetAxis("Horizontal");
-
-		walkingDirection = walkingDirection.normalized;
-		walkingDirection *= Input.GetKey(KeyCode.LeftShift) ? runningSpeed : walkingSpeed;
-
-		rigidbody.MovePosition(transform.position + walkingDirection * Time.fixedDeltaTime);
-	}
+	// private void Move()
+	// {
+	// 	var walkingDirection = Vector3.zero;
+	//
+	// 	walkingDirection += Vector3.up * Input.GetAxis("Vertical");
+	// 	walkingDirection += Vector3.right * Input.GetAxis("Horizontal");
+	//
+	// 	walkingDirection = walkingDirection.normalized;
+	// 	walkingDirection *= Input.GetKey(KeyCode.LeftShift) ? runningSpeed : walkingSpeed;
+	//
+	// 	rigidbody.MovePosition(transform.position + walkingDirection * Time.fixedDeltaTime);
+	// }
 }
